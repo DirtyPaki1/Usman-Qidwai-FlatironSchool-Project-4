dfc347bc7ce28467678d132737c4b49a5cdb96ad
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-11-30 17:07:50 -0500

    Almost

--- video_game_management/client/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const apiUrl = 'http://localhost:5000/api';
-
-export const fetchGames = () => axios.get(`${apiUrl}/games`);
-export const fetchGame = (id) => axios.get(`${apiUrl}/games/${id}`);
-export const addGame = (gameData) => axios.post(`${apiUrl}/games`, gameData);
